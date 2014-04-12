81abfe407ef3176ea340a4f8e954fe078ba6ed2e
Steve Calamia <steve@calamia.net>
2014-04-12 19:45:35 -0400

    added Venue Meta Data

--- lib/venues.php
+++ lib/venues.php
@@ -29,4 +29,62 @@ function create_venue_post_type() {
 
 	register_post_type( 'fwddc_venue', $args);
 }
-?>
+
+/**
+ * Venues Meta Boxes
+ *  we need:
+ *      title, thumbnail, url, address
+**/
+function add_venues_meta_boxes( $post ) {
+    add_meta_box( 'fwddc_venue_url', __('Venue\'s Web Page', 'roots'), 'fwddc_venue_meta', 'fwddc_venue', 'normal', 'default', array( 'name' => 'url', 'prefix' => '' ) );
+    add_meta_box( 'fwddc_venue_address', __('Venue Address', 'roots'), 'fwddc_venue_meta', 'fwddc_venue', 'normal', 'default', array( 'name' => 'address', 'prefix' => '' ) );
+}
+add_action( 'add_meta_boxes', 'add_venues_meta_boxes');
+
+function fwddc_venue_meta( $post, $meta_name ) {
+    $name = $meta_name['args']['name'];
+    $action = 'fwddc_venue_'.$name.'_meta_box';
+    $nonce_name = 'fwddc_venue_'.$name.'_meta_box_nonce';
+    $input_name = 'fwddc_'.$name;
+    wp_nonce_field( $action, $nonce_name );
+
+    $text = get_post_meta( $post->ID, '_'.$input_name, TRUE );
+
+    echo '<label for="'.$input_name.'">'.$meta_name['args']['prefix'].'</label><input type="text" name="'.$input_name.'" id="'.$input_name.'" value="'.$text.'" style="width: 80%;" />';
+}
+
+function fwddc_save_venue_meta_box_data( $post_id ) { 
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return;
+    } 
+
+    $meta_boxes = array(
+        'url',
+        'address'
+    );
+
+    foreach ($meta_boxes as $box) {
+        $action = 'fwddc_venue_'.$box.'_meta_box';
+        $nonce = $action.'_nonce';
+ 
+        if ( ! isset( $_POST[$nonce] ) ) {
+            continue;
+        }
+        if ( ! wp_verify_nonce( $_POST[$nonce], $action ) ) {
+            continue;
+        }   
+        if ( ! isset( $_POST['fwddc_'.$box] ) ) {
+            continue;
+        }
+        $meta_data = sanitize_text_field( $_POST['fwddc_'.$box] );
+        update_post_meta( $post_id, '_fwddc_'.$box, $meta_data );
+    }
+}
+add_action( 'save_post', 'fwddc_save_venue_meta_box_data' );
+
+
