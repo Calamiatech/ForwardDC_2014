b288965ff89c5440a2072f545fcbee251efccc35
Steve Calamia <steve@calamia.net>
2014-04-12 05:29:00 -0400

    Added Bootstrap DatePicker, Artist Isotope mods

--- templates/artist-isotope.php
+++ templates/artist-isotope.php
@@ -1,7 +1,26 @@
 
-<article <?php post_class(array('artist', 'col-sm-6','col-md-4','col-lg-2','isotope-ready')); ?>>    
+<?php
+/* Post Container Styles */
+$post_classes = array(
+	'artist',
+	
+	'col-sm-6',
+	'col-md-4',
+	'col-lg-2',
+
+	'isotope-ready',
+	);
+
+$genres = get_the_terms( $post->ID, 'fwddc_artist_genre' );
+if ($genres && ! is_wp_error( $genres )) {
+	foreach ($genres as $genre) {
+		$post_classes[] = $genre->name;
+	}
+}
+?>
+<article <?php post_class($post_classes); ?>>    
 	<div id="artist-content">
-		<h4 class="entry-title"><?php the_title(); ?></h4>
 	    <?php the_post_thumbnail('large',array('class'=>'img-responsive')); ?>
+		<h4 class="artist-name"><?php the_title(); ?></h4>
 	</div>
 </article>
