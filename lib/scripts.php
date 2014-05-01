db55a409cfcc3722b63cb4ca16269fcbcf533f2d
Steve Calamia <steve@calamia.net>
2014-05-01 17:07:02 -0400

    Tagged Release: v0.2

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f3380c0b3b664f7220a9b2af41a9bf92');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9bad487a44b3af754feeae5293a06f31');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
