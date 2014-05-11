afa24b8d6cedac6813aa9ab3cc24cde8c81a87c4
Steve Calamia <steve@calamia.net>
2014-05-11 13:39:03 -0400

    tiny css improvements

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b1b7fe05c3273b6c2af59ebfd9a83297');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f7ad415356af09a47c7d15e6d266875d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
